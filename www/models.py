f9f2f981c593219a5a6c2bdbb17ab0e35860aad6
Michael <15669909676@163.com>
2020-12-15 16:31:19 +0800

    dev

--- www/models.py
+++ www/models.py
@@ -8,18 +8,32 @@ def next_id():
 
 class User(Model):
     __table__ = 'users'
-    id = StringField(primary_key=True, default=next_id(), ddl='varchar(50)')
-    user_id =StringField(ddl='varchar(50)')
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    email = StringField(ddl='varchar(50)')
+    passwd = StringField(ddl='varchar(50)')
+    admin = StringField(ddl='varchar(50)')
+    name = StringField(ddl='varchar(50)')
+    image = StringField(ddl='varchar(500)')
+    create_at = FloatField(default=time.time)
+
+
+class Blog(Model):
+    __table__ = 'blogs'
+
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
     user_name = StringField(ddl='varchar(50)')
     user_image = StringField(ddl='varchar(500)')
     name = StringField(ddl='varchar(50)')
-    summary = StringField(ddl='varchar(100)')
+    summary = StringField(ddl='varchar(200)')
     content = TextField()
     create_at = FloatField(default=time.time)
 
 
 class Comment(Model):
     __table__ = 'comments'
+
     id = StringField(primary_key=True, default=next_id(), ddl='varchar(50)')
     blog_id = StringField(ddl='varchar(50)')
     user_id = StringField(ddl='varchar(50)')
